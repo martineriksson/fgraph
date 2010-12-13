579b9babf847313326f99be8d3502b4033607dc8
Martin Eriksson <martin@artilect.se>
2010-12-13 04:45:30 +0100

    Removed verbose comments (preserved in generated docs).

--- fgraph.rb
+++ fgraph.rb
@@ -76,39 +76,13 @@ module FGraph
     
     # Single object query.
     # 
-    #   # Users: https://graph.facebook.com/btaylor  (Bret Taylor)
-    #   FGraph.object('btaylor')
-    #
-    #   # Pages: https://graph.facebook.com/cocacola (Coca-Cola page)
-    #   FGraph.object('cocacola')
-    #
-    #   # Fields selection with metadata
-    #   FGraph.object('btaylor', :fields => 'id,name,picture', :metadata => 1)
-    #
-    #   # Page photos
-    #   FGraph.object('/cocacola/photos')
-    #   photos = FGraph.object_photos('cocacola')
-    #   
-    #   # Support id from object hash
-    #   friend = { 'name' => 'Mark Zuckerberg', 'id' => '4'}
-    #   friend_details = FGraph.object(friend)
     def object(id, options={})
       id = self.get_id(id)
       perform_get("/#{id}", options)
     end
   
-    # call-seq:
-    #   FGraph.objects(id, id)
-    #   FGraph.objects(id, id, options_hash)
-    #
     # Multiple objects query.
     # 
-    #   # Multiple users select: https://graph.facebook.com?ids=arjun,vernal
-    #   FGraph.objects('arjun', 'vernel')
-    #
-    #   # Filter fields: https://graph.facebook.com?ids=arjun,vernal&fields=id,name,picture
-    #   FGraph.objects('arjun', 'vernel', :fields => 'id,name,picture')
-    #
     def objects(*args)
       options = args.last.is_a?(Hash) ? args.pop : {}
     
@@ -123,21 +97,8 @@ module FGraph
       perform_get("/", options)
     end
   
-    # call-seq:
-    #   FGraph.me(category)
-    #   FGraph.me(category, options_hash)
-    # 
     # Returns current user object details.
     # 
-    # <tt>category</tt> - <tt>friends|home|feed|likes|movies|books|notes|photos|videos|events|groups</tt>
-    #
-    #   # Current user: https://graph.facebook.com/me?access_token=...
-    #   FGraph.me(:access_token => '...')
-    # 
-    #   # Current user's friends: https://graph.facebook.com/me/friends?access_token=...
-    #   FGraph.me('friends', :access_token => '...')
-    #   FGraph.me_friends(:access_token => '...')
-    #
     def me(*args)
       options = args.last.is_a?(Hash) ? args.pop : {}
       category = args.shift
@@ -151,24 +112,6 @@ module FGraph
     # user's behalf. Returns Oauth Authorization URL, redirect to this URL to allow user to authorize your
     # application from Facebook.
     #
-    # <tt>client_id</tt> - Application ID
-    # <tt>redirect_uri</tt> - Needs to begin with your app's Connect URL. For instance, if your Connect URL 
-    # is http://www.example.com then your redirect URI could be http://www.example.com/oauth_redirect.
-    # <tt>scope (optional)</tt> -
-    #
-    # ==== Options
-    # * <tt>scope</tt> -  Extended permission required to fetch private data or request permision to 
-    # publish to Facebook on a user's behalf.
-    # * <tt>display</tt> - Other display type for authentication/authorization form, i.e. popup, touch.
-    #
-    #   # https://graph.facebook.com/oauth/authorize?
-    #   #   client_id=...&
-    #   #   redirect_uri=http://www.example.com/oauth_redirect&
-    #   #   scope=publish_stream
-    #
-    #   FGraph.oauth_authorize_url('[client id]', 'http://www.example.com/oauth_redirect', :scope => 
-    #     'publish_stream')
-    #
     def oauth_authorize_url(client_id, redirect_uri, options={})
       self.format_url('/oauth/authorize', {
         :client_id => client_id,
@@ -178,27 +121,6 @@ module FGraph
   
     # Return OAuth access_token. There are two types of access token, user access token and application 
     # access token.
-    #
-    # User access_token requires <tt>code</tt> and and <tt>redirect_uri</tt> options. <tt>code</tt> is
-    # the autorization code appended as query string to redirect URI when accessing oauth authorization URL.
-    #
-    #   # https://graph.facebook.com/oauth/access_token?
-    #   #   client_id=...&
-    #   #   client_secret=...&
-    #   #   redirect_uri=http://www.example.com/oauth_redirect&
-    #   #   code=...
-    #   FGraph.oauth_access_token('[client id]', '[client secret]', 
-    #      :redirect_uri => ''http://www.example.com/oauth_redirect', 
-    #      :code => '[authorization code]')
-    #
-    # Application access token requires <tt>:type => 'client_cred'</td> option. Used to access application
-    # insights data.
-    #
-    #   # https://graph.facebook.com/oauth/access_token?
-    #   #   client_id=...&
-    #   #   client_secret=...&
-    #   #   type=client_cred
-    #   FGraph.oauth_access_token('[client id]', '[client secret]', :type => 'client_cred')
     #
     def oauth_access_token(client_id, client_secret, options={})
       url = self.format_url('/oauth/access_token', {
@@ -222,41 +144,12 @@ module FGraph
   
     # Publish to Facebook, you would need to be authorized and provide access token.
     #
-    #   # Post to user's feed.
-    #   #   curl -F 'access_token=...' \
-    #   #     -F 'message=Hello, Arjun. I like this new API.' \
-    #   #     https://graph.facebook.com/arjun/feed
-    #   FGraph.publish('arjun/feed', :message => 'Hello, Arjun. I like this new API.', 
-    #     :access_token => '...')
-    #   FGraph.publish_feed('arjun', :message => '...', :access_token => '...')
-    #   FGraph.publish_feed('me', ':message => '...', :access_token => '...')
-    #
-    # ==== Options
-    #
-    #   Method                Description                             Options
-    #   -------------------------------------------------------------------------------------
-    #   /PROFILE_ID/feed      write to the given profile's feed/wall  :message, :picture, 
-    #                                                                 :link, :name, description
-    #   /POST_ID/comments     comment on the given post               :message
-    #   /POST_ID/likes        like the given post                     none
-    #   /PROFILE_ID/notes     write a note on the given profile       :message, :subject
-    #   /PROFILE_ID/links     write a link on the given profile       :link, :message
-    #   /EVENT_ID/attending   attend the given event                  none
-    #   /EVENT_ID/maybe       maybe attend the given event            none
-    #   /EVENT_ID/declined    decline the given event                 none
-    # 
     def publish(id, options={})
       id = self.get_id(id)
       self.perform_post("/#{id}", options)
     end
   
     # Delete objects in the graph.
-    #
-    #   # DELETE https://graph.facebook.com/ID?access_token=... HTTP/1.1
-    #   
-    #   FGraph.remove('[ID]')
-    #   FGraph.remove('[ID]/likes')
-    #   FGraph.remove_likes('[ID]')
     #
     def remove(id, options={})
       id = self.get_id(id)
@@ -265,15 +158,6 @@ module FGraph
   
     # Search over all public objects in the social graph.
     # 
-    #   # https://graph.facebook.com/search?q=watermelon&type=post
-    #   FGraph.search('watermelon', :type => 'post')
-    #   FGraph.search_post('watermelon')
-    #
-    # ==== Options
-    # * <tt>type</tt> - <tt>album|event|group|link|note|page|photo|post|status|user|video</tt>
-    # * <tt>limit</tt> - max no of records
-    # * <tt>offset</tt> - offset
-    # * <tt>until</tt> - since (a unix timestamp or any date accepted by strtotime, e.g. yesterday)
     def search(query, options={})
       self.perform_get("/search", {
         :q => query
@@ -282,16 +166,6 @@ module FGraph
   
     # Download insights data for your application.
     #
-    #   # https://graph.facebook.com/[client_id]/insights?access_token=...
-    #   FGraph.insights('[client_id]', '[app_access_token]')
-    #
-    #   # https://graph.facebook.com/[client_id]/insights/application_api_call/day?access_token=...
-    #   FGraph.insights('[client_id]', '[app_access_token]', :metric_path => 'application_api_call/day')
-    # 
-    # ==== Options
-    # * <tt>metric_path</tt> - e.g. application_api_calls/day
-    # * <tt>since</tt> - since (a unix timestamp or any date accepted by strtotime, e.g. yesterday)
-    # * <tt>until</tt> - until (a unix timestamp or any date accepted by strtotime, e.g. yesterday)
     def insights(client_id, app_access_token, options={})
       metric_path = options.delete(:metric_path)
     
@@ -422,24 +296,6 @@ module FGraph
     # Initialize Client with default options, so options are not required to be passed
     # when calling respective Facebook Graph API methods.
     # 
-    # ==== Options
-    # * <tt>client_id</tt> - Application ID
-    # * <tt>client_secret</tt> - Application Secret
-    # * <tt>access_token</tt> - Access token, required to publish to Facebook Graph or access
-    #   current user profile.
-    # * <tt>app_access_token</tt> - Application access token, required to access Facebook insights.
-    #   Auto generated if client_id and client_secret option are provided.
-    #
-    #   # Initialize with default options
-    #   fg_client = FGraph::Client.new(:client_id => '...', :client_secret => '...')
-    #   fg_client.oauth_authorize_url('[redirect uri]', :scope => 'publish_stream')
-    #   fg_client.oauth_access_token('[redirect uri]', '[authorization code]')
-    #   
-    #   # Intialize with access token
-    #   fg_client = FGraph::Client.new(:access_token => '...')
-    #   fg_client.me
-    #   fg.client.publish_feed('herryanto', :message => 'Cool!')
-    #
     def initialize(options={})
       @options = options
     end
